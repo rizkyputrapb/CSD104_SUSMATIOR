637c4ba6a961887071ee37f64e10f394be73e974
fajarmuhamad616 <fajarmuhamad616@gmail.com>
2021-11-25 13:36:18 +0700

    Adding feat landing page

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -2,9 +2,12 @@
 Attributes
 - Icons made by smashicons
 */
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:susmatior_app/constants/colors_constants.dart';
+import 'package:susmatior_app/ui/screens/landing/landing_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/splash_screen';
@@ -15,6 +18,24 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void onStartTime() {
+    const _duration = Duration(seconds: 2);
+    Timer(_duration, navigationToLanding);
+  }
+
+  @override
+  void initState() {
+    onStartTime();
+    super.initState();
+  }
+
+  void navigationToLanding() {
+    Navigator.pushReplacementNamed(
+      context,
+      LandingScreen.routeName,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
