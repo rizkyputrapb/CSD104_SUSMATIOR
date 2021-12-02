6d64356608ac40bc4b5e3ff035140fbd632e1da8
rizky <rizkyputrapb@gmail.com>
2021-12-02 17:35:46 +0700

    add firebase integration to Android

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -4,6 +4,7 @@ Attributes
 */
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:susmatior_app/constants/colors_constants.dart';
@@ -11,6 +12,7 @@ import 'package:susmatior_app/ui/screens/landing/landing_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/splash_screen';
+
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
@@ -18,6 +20,27 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        print("firebase is initialized");
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
   void onStartTime() {
     const _duration = Duration(seconds: 2);
     Timer(_duration, navigationToLanding);
@@ -25,6 +48,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
+    initializeFlutterFire();
     onStartTime();
     super.initState();
   }
@@ -40,37 +64,42 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/img_splash_picture.png',
-                width: MediaQuery.of(context).size.width / 2,
-                height: MediaQuery.of(context).size.height / 2,
-              ),
-              Text(
-                'SUSMATIOR',
-                style: GoogleFonts.montserrat(
-                  color: primaryColor,
-                  fontSize: 36.0,
-                  fontWeight: FontWeight.w500,
+        child: (() {
+          if(_error) {
+            return const SnackBar(content: Text("Error initializing firebase"));
+          }
+          return Center(
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/img_splash_picture.png',
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: MediaQuery.of(context).size.height / 2,
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 7,
-              ),
-              Text(
-                'Suspicious Information\nObservers',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.montserrat(
-                  color: primaryColor,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w400,
+                Text(
+                  'SUSMATIOR',
+                  style: GoogleFonts.montserrat(
+                    color: primaryColor,
+                    fontSize: 36.0,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 7,
+                ),
+                Text(
+                  'Suspicious Information\nObservers',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.montserrat(
+                    color: primaryColor,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }()),
       ),
     );
   }
