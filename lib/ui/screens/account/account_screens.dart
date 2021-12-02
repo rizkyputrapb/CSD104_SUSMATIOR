9753d8703e480c3aa0d47e32cb45e2808df7a217
fajarmuhamad616 <fajarmuhamad616@gmail.com>
2021-12-02 23:16:21 +0700

    separated widget for account screens to folder widget

--- lib/ui/screens/account/account_screens.dart
+++ lib/ui/screens/account/account_screens.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:susmatior_app/constants/padding_constants.dart';
+import 'package:susmatior_app/ui/screens/account/widgets/card_avatar_widget.dart';
+import 'package:susmatior_app/ui/screens/account/widgets/card_widget.dart';
 import 'package:susmatior_app/ui/screens/widgets/appbar_widget.dart';
 import 'package:susmatior_app/ui/screens/widgets/btn_expanded_widget.dart';
 
@@ -14,116 +16,17 @@ class AccountScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Card(
-            elevation: 0.0,
-            color: Color(0xFFECF0F1),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 10.0,
-              vertical: 2.0,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        radius: 27,
-                        child: ClipOval(
-                          child: Image.asset(
-                            'assets/icons/ic_user_scam.png',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 4,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'John Doe',
-                            style: GoogleFonts.montserrat(
-                              fontSize: 19.0,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xFF2F2E41),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 8.0,
-                          ),
-                          Text(
-                            'Change Photo Profile',
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: GoogleFonts.montserrat(
-                              fontSize: 14.0,
-                              color: Color(0xFF428DFF),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          CardAvatarAccount(
+            imageUserAccount: 'assets/icons/ic_user_scam.png',
+            titleText: 'John Doe',
+            childText: 'Change Photo Profile',
           ),
-          Card(
-            elevation: 0.0,
-            color: Color(0xFFECF0F1),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 10.0,
-              vertical: 2.0,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 4,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            'Email',
-                            style: GoogleFonts.montserrat(
-                              fontSize: 19.0,
-                              fontWeight: FontWeight.w500,
-                              color: Color(0xFF2F2E41),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 8.0,
-                          ),
-                          Text(
-                            'abcdef@gmail.com',
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 2,
-                            style: GoogleFonts.montserrat(
-                              fontSize: 14.0,
-                              color: Color(0xFF2F2E41),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          CardAccount(
+            titleText: 'Email',
+            childText: 'abcdef@gmail.com',
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(padding_8),
             child: InkWell(
               onTap: () {},
               child: ButtonRectangleExpanded(textButton: 'Logout'),
